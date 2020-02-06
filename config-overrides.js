db2a9f77552b581a37fa68b78705266ce856c27d
Watch1k <host.sf@gmail.com>
2020-02-06 17:07:56 +0200

    fix antd icons size bundle

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,9 @@
-const { override, fixBabelImports } = require('customize-cra')
+const {
+  override,
+  fixBabelImports,
+  addBundleVisualizer,
+} = require('customize-cra')
+const path = require('path')
 
 module.exports = override(
   fixBabelImports('import', {
@@ -6,4 +11,17 @@ module.exports = override(
     libraryDirectory: 'es',
     style: 'css',
   }),
+  function(config) {
+    const alias = config.resolve.alias || {}
+    alias['@ant-design/icons/lib/dist$'] = path.resolve(
+      __dirname,
+      './src/icons.js'
+    )
+    config.resolve.alias = alias
+    return config
+  },
+  addBundleVisualizer({
+    analyzerMode: 'static',
+    reportFilename: 'report.html',
+  })
 )
---
+++ src/icons.js
@@ -0,0 +1,3 @@
+export { default as CheckCircleOutline } from '@ant-design/icons/lib/outline/CheckCircleOutline'
+
+export { default as UnorderedListOutline } from '@ant-design/icons/lib/outline/UnorderedListOutline'
