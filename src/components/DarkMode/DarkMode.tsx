6a69bc9d46cb6e76cec167cfb5cf70888c5d313e
filshyn <alexandr.filshyn@heathmont.net>
2020-02-06 15:06:21 +0200

    test

--- src/components/DarkMode/DarkMode.tsx
+++ src/components/DarkMode/DarkMode.tsx
@@ -3,8 +3,6 @@ import { Switch } from 'antd'
 import { useDarkModeConnect } from '../../useConnect/darkMode'
 import { darkModeStyles } from './DarkModeStyles'
 
-console.log('test')
-
 const DarkMode = () => {
   const { setDarkMode } = useDarkModeConnect()
   const handleChange = (checked: boolean) => {
