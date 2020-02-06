5ddcb6f2e5ec08ea9d7abf0e0c5ee5303f3016dd
filshyn <alexandr.filshyn@heathmont.net>
2020-02-06 15:00:28 +0200

    testt2

--- src/components/DarkMode/DarkMode.tsx
+++ src/components/DarkMode/DarkMode.tsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import { Switch } from 'antd'
 import { useDarkModeConnect } from '../../useConnect/darkMode'
-import { darkModeStyles } from './DarkModeStyles';
+import { darkModeStyles } from './DarkModeStyles'
 
 const DarkMode = () => {
-  const { setDarkMode } = useDarkModeConnect();
+  const { setDarkMode } = useDarkModeConnect()
   const handleChange = (checked: boolean) => {
-    setDarkMode(checked);
+    setDarkMode(checked)
   }
 
   return (
     <div style={darkModeStyles}>
       <Switch onChange={handleChange} data-testid='darkMode' />
     </div>
-  );
+  )
 }
 
-export default DarkMode;
+export default DarkMode
